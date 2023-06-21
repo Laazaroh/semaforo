c219f0b6ff5a31d1dda3ef36a9cec54d861ce798
LaazaroH <henriquelazaro1902@gmail.com>
2023-06-21 17:59:48 -0300

    🎉

--- semaforo.js
+++ semaforo.js
@@ -1,19 +1,20 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 let colorIndex = 0
+let intervalID = null;
+
 
 const trafficLight = (event) =>{
-     turnOn[event.target.id]();
+    clearInterval()
+    turnOn[event.target.id]();
 }
 const nextIndex = () =>{
     if (colorIndex < 2){
         colorIndex++
-        
+
     }else{
-        colorIndex = 0
+        colorIndex = 0;
     }
-
-
 }
 
 const changeColor = () =>{
@@ -21,12 +22,17 @@ const changeColor = () =>{
     let color = colors[colorIndex];
     turnOn[color]();
     nextIndex();
+    turnOn['automatic']();
+}
+const stopAutomatic = ( )=>{
+    clearInterval(intervalID)
 }
 const turnOn ={
     'red': () => img.src ='./img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => setTimeout(changeColor, 100)
+    'automatic': () => intervalID = setTimeout(changeColor, 600)
 }
+
 
 buttons.addEventListener('click', trafficLight)
